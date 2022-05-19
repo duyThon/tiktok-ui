3378431d718141d601ee6501cc475dab6da59262
Ng Duy Thong <cgl.ngduythong@gmail.com>
2022-05-20 00:44:45 +0700

    cuoc song nay

--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,6 +1,6 @@
 import Header from "~/components/Layout/components/Header";
 
-function DefaultLayut({ children }) {
+function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
@@ -13,4 +13,4 @@ function DefaultLayut({ children }) {
     );
 }
 
-export default DefaultLayut;
+export default HeaderOnly;
